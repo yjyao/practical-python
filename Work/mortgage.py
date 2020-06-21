617371d0ab0852937aa860da80a7f668c3c24995
Jy <yjyao1112@gmail.com>
2020-06-21 16:29:47 -0700

    Ex1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -14,10 +14,12 @@ monthly_pay = 2684.11
 total_paid = 0
 
 for i in count(1):
-  pay = monthly_pay
+  extra_pay = 0
   if extra_payment_start_month <= i <= extra_payment_end_month:
-    pay += extra_payment
-  principal = max(principal * (1 + interest_rate/12) - pay, 0)
+    extra_pay += extra_payment
+  principal *= (1 + interest_rate/12)
+  pay = min(principal, monthly_pay+extra_pay)
+  principal -= pay
   total_paid += pay
   print(i, total_paid, principal)
   if not principal:
