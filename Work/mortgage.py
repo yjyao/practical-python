a36bd882b9ce08ce437085af09a1dafca6a8b9d1
Jy <yjyao1112@gmail.com>
2020-06-21 16:14:23 -0700

    Ex1.7

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,14 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500_000
+interest_rate = 0.05
+monthly_pay = 2684.11
+total_paid = 0
+
+while principal:
+  principal = max(principal * (1 + interest_rate/12) - monthly_pay, 0)
+  total_paid += monthly_pay
+
+print(f'Total paid: ${round(total_paid, 2)}')
