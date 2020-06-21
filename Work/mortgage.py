bb305e94d52cf7527f04eb451ff7b4e48fbf55ba
Jy <yjyao1112@gmail.com>
2020-06-21 16:19:38 -0700

    Ex1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -2,13 +2,20 @@
 #
 # Exercise 1.7
 
+from itertools import count
+
 principal = 500_000
 interest_rate = 0.05
 monthly_pay = 2684.11
 total_paid = 0
 
-while principal:
-  principal = max(principal * (1 + interest_rate/12) - monthly_pay, 0)
-  total_paid += monthly_pay
+for i in count(1):
+  pay = monthly_pay
+  if i <= 12:
+    pay += 1000
+  principal = max(principal * (1 + interest_rate/12) - pay, 0)
+  total_paid += pay
+  if not principal:
+    break
 
-print(f'Total paid: ${round(total_paid, 2)}')
+print(f'Total paid ${round(total_paid, 2)} over {i} months')
